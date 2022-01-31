fba828a19b2b4f488a0716eb138e017508487963
Wellington Pires <wellington.goncalves@live.com>
2022-01-31 22:08:12 +0000

    First commit

---
+++ src/main/java/br/com/furafila/establishmentapp/FuraFilaEstablishmentAppApplication.java
@@ -0,0 +1,13 @@
+package br.com.furafila.establishmentapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuraFilaEstablishmentAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FuraFilaEstablishmentAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/furafila/establishmentapp/FuraFilaEstablishmentAppApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.furafila.establishmentapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FuraFilaEstablishmentAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
